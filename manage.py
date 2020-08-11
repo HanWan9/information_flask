26ecebc4ca81c18c17c1e32b4924c422efc63d94
HanWang <hwang_wl@gwu.edu>
2020-08-11 01:44:25 -0400

    项目配置创建&数据库配置

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+
 class Config(object):
-    "项目配置"
+    # 项目配置
     DEBUG = True
     # 为mysql添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information_flask"
