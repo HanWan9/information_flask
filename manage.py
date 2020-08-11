905f99504f869c6d50d6fa8278db3a9c3ff1b1de
Wang Han <han@hunger.myfiosgateway.com>
2020-08-11 01:33:58 -0400

    改名manage.py

---
+++ manage.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+class Config(object):
+    "项目配置"
+    DEBUG = True
+#     为mysql添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information_flask"
+
+app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+# 初始化数据库
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return 'index333'
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
