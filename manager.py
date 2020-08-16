b4e12faaa6ae35e60592a7f1856e750e139b6354
HanWang <hwang_wl@gwu.edu>
2020-08-16 17:07:09 -0400

    集成日志到项目

--- manager.py
+++ manager.py
@@ -1,6 +1,9 @@
 from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+import logging
+from flask import current_app
+from flask import session
 
 
 from info import create_app,db
@@ -19,7 +22,17 @@ manager.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
-    session["name"] = "itheima"
+    # session["name"] = "itheima"
+
+    # 测试打印日志
+    logging.debug('测试debug')
+    logging.warning('测试warning')
+    logging.error('测试error')
+    logging.fatal('测试fatal')
+
+    # current_app.logger.error('测试error')
+
+
     return 'index'
 
 
